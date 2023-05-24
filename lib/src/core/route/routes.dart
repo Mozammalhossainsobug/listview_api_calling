b195f1db0c12f887477e54142182ad113de56ee8
Mozammalhossainsobug <mozammalruet@gmail.com>
2023-05-24 18:09:30 +0600

    final selection

--- lib/src/core/route/routes.dart
+++ lib/src/core/route/routes.dart
@@ -3,13 +3,13 @@ import 'package:listview_api_calling/src/feature/data_showing/pages/data_showing
 import 'package:listview_api_calling/src/core/route/routes_constant.dart';
 
 
-Route<dynamic> generateRoute(RouteSettings settings) {
+Route<dynamic>? generateRoute(RouteSettings settings) {
 
   switch (settings.name) {
-    // case dataShowingPage:
-    //   return MaterialPageRoute(builder: (context) => MyListView());
+     case dataShowingPage:
+      return MaterialPageRoute(builder: (context) => MyListView());
 
      default:
-       return MaterialPageRoute(builder: (context) => MyListView());
+       return null;
   }
 }
